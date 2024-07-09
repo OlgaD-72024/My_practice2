ac197a8f78d6795fb79db0979cfb5f10dbb3f02a
OlgaD-72024 <yachhik@gmail.com>
2024-07-09 22:26:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ name = "Olga"
 print("Name: " + name)
 age = 36
 print("Age: ", age)
-age = 30 + 6
-print("New age: ", age)
+new_age = age + 1
+print("New age: ", new_age)
 is_student = True
 print("Is Student: ", is_student)
